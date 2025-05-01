ff4b1cb4c7e95a0599b71be34eaf49b162e3990b
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-04-30 19:34:02 -0700

    work on proj

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,35 @@
 import { fetchJSON, renderProjects } from "../global.js";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON("../lib/projects.json");
 const projectsContainer = document.querySelector(".projects");
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+d3.select("svg").append("path").attr("d", arc).attr("fill", "red");
+let data = [1, 2];
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = ["gold", "purple"];
+
+arcs.forEach((arc, idx) => {
+  d3.select("svg").append("path").attr("d", arc).attr("fill", colors[idx]); // Fill color using the colors array
+});
 
 renderProjects(projects, projectsContainer, "h2");
