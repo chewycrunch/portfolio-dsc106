f63e174d20e9f54ff0e5c6e1a9f1e4e07ad251b7
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-05-04 22:40:36 -0700

    work on lab 6

--- global.js
+++ global.js
@@ -15,6 +15,7 @@ const pages = [
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
   { url: "resume/", title: "Resume" },
+  { url: "meta/", title: "Meta" },
   { url: "https://github.com/chewycrunch", title: "Github Profile" },
 ];
 
